5d0a439d36750114e6510e1c108f309bddd0ff04
norman <norman@enclave.vn>
2020-03-04 18:21:17 +0700

    fix bug binding data for Kendo UI Grid from API getway

--- FrontEnd/WebApp/ClientApp/src/app/app.module.ts
+++ FrontEnd/WebApp/ClientApp/src/app/app.module.ts
@@ -9,6 +9,10 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
@@ -16,7 +20,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -25,8 +29,10 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+      { path: 'fetch-data', component: FetchDataComponent }
+    ]),
+    GridModule,
+    BrowserAnimationsModule
   ],
   providers: [{ provide: 'API_BASE_URL', useFactory: getBaseAPIGetwayUrl, deps: [] }],
   bootstrap: [AppComponent]
--- FrontEnd/WebApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ FrontEnd/WebApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,22 +1,25 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html'
 })
+
 export class FetchDataComponent {
+
   public customers: Customer[];
 
   constructor(http: HttpClient, @Inject('API_BASE_URL') baseUrl: string) {
     http.get<Customer[]>(baseUrl + 'api/customer').subscribe(result => {
-      this.customers = result;
+      this.customers = <Customer[]>result;
     }, error => console.error(error));
   }
 }
 
 interface Customer {
-  id: string;
+  id: number;
   name: string;
   address: string;
   business: string;
