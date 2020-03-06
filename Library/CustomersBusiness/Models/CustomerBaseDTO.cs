a7c7487fe07b125b50b1fd2208bc50ece83dc5f5
norman <norman@enclave.vn>
2020-03-06 09:44:12 +0700

    change Customer Business into Library folder

---
+++ Library/CustomersBusiness/Models/CustomerBaseDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ProtoBuf;
+
+namespace CustomersBusiness.Models
+{
+    [ProtoContract]
+    [ProtoInclude(101, typeof(CustomerDTO))]
+    public abstract class CustomerBaseDTO
+    {
+        [ProtoMember(2)]
+        [Required]
+        public string Name { get; set; }
+        [ProtoMember(3)]
+        [Required]
+        public string Address { get; set; }
+        [ProtoMember(4)]
+        [Required]
+        public string Business { get; set; }
+    }
+}
